dcfce378b8178131f8119c6be7bc7b1d021a0e4c
ibrahim <nsism99@gmail.com>
2023-08-10 08:37:47 +0300

    text

---
+++ day4/mein.js
@@ -0,0 +1,57 @@
+// num to str 
+const num_1=42;
+const numToStr=String(num_1)
+console.log(`${typeof num_1}: ${num_1}`);
+console.log(`${typeof numToStr}: ${numToStr}`);
+
+
+// str to number
+const str_2="25";
+const strtrTonum=Number(str_2)
+console.log(`${typeof str_2}: ${str_2}`);
+console.log(`${typeof strtrTonum}: ${strtrTonum}`);
+
+
+
+const str_="10";
+const strToboll=String(str_)
+console.log(`${typeof str_}: ${str_}`);
+console.log(`${typeof strToboll}: ${strToboll}`);
+
+const um=(10-"22")
+console.log(um);
+const um_1=(10 +"22")
+console.log(um_1);
+
+
+const num_2=3;
+const nuMToStr=String(num_1)
+console.log(`${typeof num_2}: ${num_2}`);
+console.log(`${typeof nuMToStr}: ${nuMToStr}`);
+
+
+// str to number
+const str_3="5";
+const strtrToNum=Number(str_3)
+console.log(`${typeof str_3}: ${str_3}`);
+console.log(`${typeof strtrToNum}: ${strtrToNum}`);
+
+const umw=(3-"5")
+console.log(umw);
+
+let text2 = true;
+console.log(text2);
+
+let tweiq = 1;
+console.log(tweiq);
+
+const ibrahim=(text2 + tweiq)
+console.log(ibrahim);
+
+
+
+const str_5="15.5";
+const strtrToflot=float(str_2)
+console.log(`${typeof str_5}: ${str_5}`);
+console.log(`${typeof strtrToflot}: ${strtrToflot}`);
+
